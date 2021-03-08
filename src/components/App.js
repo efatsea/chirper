74b5fe41de8a0d846281eb2a1eae3715de9933e4
Eleni Fatsea <efatsea@gmail.com>
2021-03-08 18:17:48 +0200

    Add the new tweet section and the liked functionality

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard'
+import LoadingBar from 'react-redux-loading'
+import NewTweet from './NewTweet'
 
 class App extends Component {
 	componentDidMount(){
@@ -10,9 +12,10 @@ class App extends Component {
   render() {
     return (
       <div>
+        <LoadingBar/>
         {this.props.loading === true
           ? null
-          : <Dashboard/>
+          : <NewTweet/>
         }
       </div>
     )
