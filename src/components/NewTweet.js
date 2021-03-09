8c3509a80734280d1cd470d1dbd7a1f7fe5f780a
Eleni Fatsea <efatsea@gmail.com>
2021-03-09 17:55:46 +0200

    add react router

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleAddTweet } from '../actions/tweets'
+import { Redirect } from 'react-router-dom'
 
 class NewTweet extends Component {
 	state ={
 		text: '',
+		toHome: false,
 	}
 
 	handleChange=(e)=>{
@@ -16,16 +20,24 @@ class NewTweet extends Component {
 		e.preventDefault()
 
 		const { text } = this.state
-		console.log('New Tweet', text)
+		const { dispatch, id } =this.props
+
+		dispatch(handleAddTweet(text, id))
 
 		this.setState(()=>({
-			text:''
+			text:'',
+			toHome: id ? false : true,
 		}))
 	}
 
 	render(){
 
-		const { text } = this.state
+		const { text, toHome } = this.state
+
+		if(toHome === true) {
+			return <Redirect to= '/' />
+		}
+
 		const tweetLeft = 280-text.length
 
 		return(
@@ -59,4 +71,4 @@ class NewTweet extends Component {
 	}
 }
 
-export default NewTweet
+export default connect()(NewTweet)
